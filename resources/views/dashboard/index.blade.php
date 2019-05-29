472dd93d86c77078a211301e19df88a54b291816
Zaeem Tarrar <zaeemtarrar3@gmail.com>
2019-05-29 16:44:28 -0700

    Admin Dashboard Index File Changed by Zaeem 1

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,27 +1,23 @@
+@extends('layouts.dashboard')
 
+@section('content')
 
-        @extends('layouts.dashboard')
+<div class="page-header">
+    <h1>
+        <i class="fa fa-users"></i>
+        &nbsp;
+        Dashboard
+    </h1>
+</div><!-- /.page-header -->
+<div class>
+    <p>
+        <b>
+            Note:
+        </b>
+        You can add your demo text here !
+    </p>
+</div>
 
-        @section('content')
-        
-        <div class="page-header">
-            <h1>
-                <i class="fa fa-users"></i>
-                &nbsp;
-                Dashboard List
-            </h1>
-        </div><!-- /.page-header -->
-        <div class >
-            <p>
-                <b>
-                    Note:
-                </b>
-                You can add your demo text here !
-            </p>
-        </div>
-        
-        {{-- @include('shared.notification') --}}
-        
-         @endsection
+{{-- @include('shared.notification') --}}
 
-        
+@endsection
